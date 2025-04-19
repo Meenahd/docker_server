c6b6307dac7dc1e4827641e6a9923b0eb444cd52
Meenahd <123823841+Meenahd@users.noreply.github.com>
2025-04-18 20:24:13 -0500

    Update main.tf

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"
-  profile= "yusuf"
+  profile= "Dennis"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "yusuf-docker-tfstate"
+  bucket = "Dennis-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
