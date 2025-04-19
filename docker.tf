1199e0d9c7ea7ce05050e3e01cd0308a9dd93bfc
Meenahd <123823841+Meenahd@users.noreply.github.com>
2025-04-18 20:27:27 -0500

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Create a remote backend for your terraform 
 terraform {
   backend "s3" {
-    bucket = "meenah-docker-tfstate"
+    bucket = "Dennis-docker-tfstate"
     dynamodb_table = "app-state"
     key    = "LockID"
     region = "us-east-1"
